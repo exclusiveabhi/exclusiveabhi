f6cdd3c0a12f455a44eb57a5bdac44dab759ec3f
exclusiveabhi <89240963+exclusiveabhi@users.noreply.github.com>
2022-09-04 20:37:14 +0530

    Add files via upload

---
+++ layouts/js/app.js
@@ -0,0 +1,76 @@
+/* Template Name: Cristino - Responsive Personal Template
+   Author: Shreethemes
+   Email: shreethemes@gmail.com
+   Website: http://www.shreethemes.in
+   Version: 1.9.0
+   Created: May 2020
+   File Description: Main JS file of the template
+*/
+
+/************************/
+/*       INDEX          */
+/*=======================
+ *  01.  Loader         *
+ *  02.  Menu           *
+ *  03.  Scrollspy      *
+ *  04.  Magnific Popup *
+ *  05.  Owl Carousel   *
+ *  06.  Back to top    *
+ *  07.  Feather Icon   *
+ =======================*/
+
+// Preloader
+window.onload = function loader() { 
+    setTimeout(() => {
+        document.getElementById('preloader').style.visibility = 'hidden';
+        document.getElementById('preloader').style.opacity = '0';
+    }, 350);
+} 
+
+
+// Menu sticky
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add("nav-sticky");
+    } else {
+        navbar.classList.remove("nav-sticky");
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
+
+// back-to-top
+var mybutton = document.getElementById("back-to-top");
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        console.log(document.body.scrollTop);
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//Feather icon
+feather.replace()
+
+// Navbar Active Class
+var spy = new Gumshoe('#navbar-navlist a', {
+    // Active classes
+    // navClass: 'active', // applied to the nav list item
+    // contentClass: 'active', // applied to the content
+    offset: 80
+});
